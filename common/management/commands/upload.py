f0e680c913acb3729769a7fcf4006c26a929e7b1
Bobognargnar <fbr.larosa@gmail.com>
2024-02-09 20:42:54 +0100

    feat: Added command line upload for multiple files

---
+++ common/management/commands/upload.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from common.models import File
+import os
+
+class Command(BaseCommand):
+    help = 'Upload one or more Data Flow files to the database. Usage: python .\manage.py upload file1 file2 <...>'
+
+    def add_arguments(self, parser):
+        parser.add_argument('args', nargs='+', type=str, help='Files to upload')
+        
+
+    def handle(self, *args, **options):
+        self.stdout.write(f"Uploading files from command line...")
+        
+        if not args:
+            self.stdout.write(f"Please specify one or more files to upload")
+            exit()
+        
+        for file in args:
+            self.stdout.write(f"{file}...")
+            filename = os.path.basename(file)
+
+            # Prevent multiple submissions of the same file
+            if not File.objects.filter(filename=filename).exists():
+                file_instance = File(filename = filename )
+                file_instance.file.save(os.path.basename(file), open(file, 'rb'))
+            else:
+                self.stdout.write(f"Duplicated file, will be ignored.")
+        
+        self.stdout.write(f"Upload completed.")
